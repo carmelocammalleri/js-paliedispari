97064b655d7fcd89ee519ed783aee7449b8810af
Melozzo <carmecamma1995@gmail.com>
2023-09-15 17:27:26 +0200

    [edit]: try to  edit function

--- js/script.js
+++ js/script.js
@@ -4,5 +4,8 @@ const parola = prompt('scrivi una parola');
 let array = [];
 array.push(parola);
 
+function reverse (){
+  let elementoDiviso = 
+}
 
-console.log(array);
+console.log(reverse(parola));
